d35c115b8bca5c1b6ecb64a32fe2a8d9ec8ddb21
Jantsen DeRoest <deroest6@hotmail.com>
2019-08-15 13:00:01 -0600

    Modified the port to work with a Heroku deployment

--- app.js
+++ app.js
@@ -210,6 +210,6 @@ app.post("/login", function(req, res) {
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+   console.log("Server is running on port 3000");
 });
