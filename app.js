59478d454f3245e30a40c9d8c338d1a9b6840d1c
Jantsen DeRoest <deroest6@hotmail.com>
2019-10-14 14:22:54 -0600

    Revised app.listening port

--- app.js
+++ app.js
@@ -210,10 +210,11 @@ app.post("/login", function(req, res){
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, function() {
-  console.log("Server started on port 3000.");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
